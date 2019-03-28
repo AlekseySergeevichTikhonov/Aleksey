b3dd2a113c8ccc2267f57dbb1645274fae0308fd
AlekseySergeevichTikhonov <lesha352001@yandex.ru>
2019-03-28 23:31:48 +0300

    Lab_2

--- Ltwo/Lab_2_3/main.cpp
+++ Ltwo/Lab_2_3/main.cpp
@@ -3,25 +3,14 @@
 using namespace std;
 int main()
 {
-    int a = 0;
-    float m[a];
-    float S = 0;
-
-    cout<<"Введите длину последовательности:";
-    cin >>a;
-    cout<<"Введите значения:";
-    for (int i=0; i<a; i++) {
-        cin>>m[i];
-    }
-    for (int i=0; i<a; i++) {
-        if(m[i]>0){
-            S=S+m[i];
+    int a;
+    int S = 0;
+    do {
+        cin>>a;
+        if (a>0){
+            S = S + a;
         }
-        else 
-            if(m[i]==0){
-            break;
-            }
-    }
-cout<<"Сумма="<< S << endl;
-return 0;
+    } while ( a!= 0);
+    cout<< S << endl;
+    return 0;
 }
